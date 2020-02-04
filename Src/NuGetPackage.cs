0b862c6ac62ef45d38b53838bde7540b38bf40c7
Curtis Carter <lokiphasma@gmail.com>
2020-02-03 21:19:37 -0600

    Initial Vulnerability Checks

---
+++ Src/NuGetPackage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace NuGetDefense
+{
+    public class NuGetPackage
+    {
+        public string Id { get; set; }
+
+        public string Version { get; set; }
+        
+        public string PackageUrl => $@"pkg:nuget/{Id}@{Version}";
+
+    }
+}
---
+++ Src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace NuGetDefense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        
+        
+        /// <summary>
+        /// Loads NuGet packages in use form packages.config or PackageReferences in the project file
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<NuGetPackage> LoadPackages()
+        {
+            if(File.Exists("packages.config"))
+            {
+                return XElement.Load("package.config").DescendantsAndSelf("Package").Select(x => new NuGetPackage()
+                    {Id = x.Attribute("id").Value, Version = x.Attribute("Version").Value});
+            }
+            else
+            {
+                return XElement.Load("ProjectFileHere").DescendantsAndSelf("PackageReference").Select(x => new NuGetPackage()
+                    {Id = x.Attribute("Include").Value, Version = x.Attribute("Version").Value});
+            }
+        }
+    }
+}
