f35d8852643a07c365dddfcf3a1b2b53a3cb4602
Marco Junge <marco.junge@inovex.de>
2020-03-15 12:51:20 +0100

    Fix: naming in tests

--- Src/NuGetDefense.Tests/PackageSources/PackagesConfigFileReaderTests.cs
+++ Src/NuGetDefense.Tests/PackageSources/PackagesConfigFileReaderTests.cs
@@ -4,17 +4,17 @@ using Xunit;
 
 namespace NuGetDefense.Tests.PackageSources
 {
-    public class PackagesConfigReaderTests
+    public class PackagesConfigFileReaderTests
     {
         [Fact]
-        public void TryReadFromPackagesConfig_ReturnsFalse_WhenFileDoesNotExist()
+        public void TryReadFromFile_ReturnsFalse_WhenFileDoesNotExist()
         {
             PackagesConfigFileReader.TryReadFromFile(@"non-existing/packages.config", out var packages).Should().BeFalse();
             packages.Should().BeEmpty();
         }
         
         [Fact]
-        public void TryReadFromPackagesConfig_ReturnsTrue_WhenFileExists()
+        public void TryReadFromFile_ReturnsTrue_WhenFileExists()
         {
             PackagesConfigFileReader.TryReadFromFile(@"TestFiles/packages.config", out var packages).Should().BeTrue();
             packages.Should().NotBeEmpty().And.HaveCount(4);
--- Src/NuGetDefense.Tests/PackageSources/ProjectFileReaderTests.cs
+++ Src/NuGetDefense.Tests/PackageSources/ProjectFileReaderTests.cs
@@ -7,14 +7,14 @@ namespace NuGetDefense.Tests.PackageSources
     public class ProjectFileReaderTests
     {
         [Fact]
-        public void TryReadFromProjectFile_ReturnsFalse_WhenFileDoesNotExist()
+        public void TryReadFromFile_ReturnsFalse_WhenFileDoesNotExist()
         {
             ProjectFileReader.TryReadFromFile(@"non/existing.csproj", out var packages).Should().BeFalse();
             packages.Should().BeEmpty();
         }
         
         [Fact]
-        public void TryReadFromProjectFile_ReturnsTrue_WhenFileExists()
+        public void TryReadFromFile_ReturnsTrue_WhenFileExists()
         {
             ProjectFileReader.TryReadFromFile(@"TestFiles/NetCoreConsoleApp.csproj", out var packages).Should().BeTrue();
             packages.Should().NotBeEmpty().And.HaveCount(1);
