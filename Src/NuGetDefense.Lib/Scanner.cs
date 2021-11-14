e9781a54d7944ac5e342b6da676fdbe1c0842bf2
Curtis Carter <lokiphasma@gmail.com>
2021-11-14 09:28:18 -0600

    Removed --tfm when targetframework is undefined

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -19,7 +19,7 @@ namespace NuGetDefense
 {
     public class Scanner
     {
-        private const string Version = "3.0.0-pre0013";
+        private const string Version = "3.0.0-pre0014";
         private static readonly string UserAgentString = @$"NuGetDefense/{Version}";
 
         private string _nuGetFile;
