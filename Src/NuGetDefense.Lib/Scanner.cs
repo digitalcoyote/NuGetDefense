ad95db45d18e18904acc00af934d926bec88de95
Curtis Carter <lokiphasma@gmail.com>
2022-10-29 23:15:10 -0500

    v3.0.8
    Bumped Dependencies

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -18,7 +18,7 @@ namespace NuGetDefense;
 
 public class Scanner
 {
-    private const string Version = "3.0.7";
+    private const string Version = "3.0.8";
     private static readonly string UserAgentString = @$"NuGetDefense/{Version}";
 
     private string _nuGetFile;
