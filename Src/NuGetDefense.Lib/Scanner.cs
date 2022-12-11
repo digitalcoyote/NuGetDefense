232cb66bac5b532f943c9264cb6e72f20e566d27
Curtis Carter <lokiphasma@gmail.com>
2022-12-11 13:19:07 -0600

    v3.0.9
    * Fixes #110 by updating NuGetDefense.GitHubAdvisoryDatabase
    * Bump versions and include readme in Nuget

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -18,7 +18,7 @@ namespace NuGetDefense;
 
 public class Scanner
 {
-    private const string Version = "3.0.8";
+    private const string Version = "3.0.9";
     private static readonly string UserAgentString = @$"NuGetDefense/{Version}";
 
     private string _nuGetFile;
