9bff095c7f4b96c81b2e95c47ae73bb40eeaef7e
digitalcoyote <lokiphasma@gmail.com>
2023-03-18 17:24:29 -0500

    Provide Default Cache Location for sqlLite cache
    As discussed in #127

--- README.md
+++ README.md
@@ -1,39 +1,38 @@
-
 # [![NuGetDefense](https://raw.githubusercontent.com/digitalcoyote/NuGetDefense/master/.github/images/logo.png)](https://digitalcoyote.github.io/NuGetDefense/)
 
 [![Join the chat at https://gitter.im/NuGetDefense/community](https://badges.gitter.im/NuGetDefense/community.svg)](https://gitter.im/NuGetDefense/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)  [![NuGet version](https://badge.fury.io/nu/NugetDefense.svg)](https://badge.fury.io/nu/NugetDefense)
 
 An MSBuildTask that checks for known vulnerabilities. Inspired by [OWASP SafeNuGet](https://github.com/OWASP/SafeNuGet).
-  
+
 ## Docs
 
 View the full documentation for NuGetDefense [here](https://digitalcoyote.github.io/NuGetDefense/)
 
 3.x preview documentation can be found by running `dotnet /path/to/NuGetDefense.dll -?`
-  
-## Features  
+
+## Features
 
 * Uses Multiple Sources to check for known vulnerabilities in third-party libraries (NuGet packages)
-  * [OSS Index](https://ossindex.sonatype.org/)
-  * [National Vulnerability Database](https://nvd.nist.gov/) (Optionally Self-Updating)
-  * [GitHub Security Advisory Database](https://nvd.nist.gov/)
-  * [Google's Open Source Vulnerabilities Database](https://osv.dev/) ([Coming Soon!](https://github.com/digitalcoyote/NuGetDefense/discussions/53))
+    * [OSS Index](https://ossindex.sonatype.org/)
+    * [National Vulnerability Database](https://nvd.nist.gov/) (Optionally Self-Updating)
+    * [GitHub Security Advisory Database](https://nvd.nist.gov/)
+    * [Google's Open Source Vulnerabilities Database](https://osv.dev/) ([Coming Soon!](https://github.com/digitalcoyote/NuGetDefense/discussions/53))
 * Simple installation/configuration: the [NuGet Package](https://www.nuget.org/packages/NuGetDefense/) is all you need.
 * dotnet Global Tool for those who want to run it manually or just in the CI
 * Transitive Dependency Checking
-  * SDK style projects only (older project format is not supported by the dotnet cli)
-  * Uses the versions resolved by the dotnet cli at build
+    * SDK style projects only (older project format is not supported by the dotnet cli)
+    * Uses the versions resolved by the dotnet cli at build
 * Project Reference Scanning
-  * Scan all projects in a hierarchy by installing NuGet Defense to the top level package
+    * Scan all projects in a hierarchy by installing NuGet Defense to the top level package
 * Allow breaking the build based on severity of vulnerability.
 * Ignore specific vulnerabilities/packages.
 * Sensitive/Internal Packages filtering
-  * Don't send packages that are sensitive/internal to remote vulnerability sources
+    * Don't send packages that are sensitive/internal to remote vulnerability sources
 * Caching to prevent excess calls and hitting rate limits on API's
 * Blocklisting NuGet Packages
 * Allowlisting NuGet Packages
 * MIT Licensed
-  * Consumable NuGet packages for bundling NuGetDefense scanners into your own software
+    * Consumable NuGet packages for bundling NuGetDefense scanners into your own software
 
 ## Requirements
 
@@ -42,14 +41,20 @@ View the full documentation for NuGetDefense [here](https://digitalcoyote.github
 * NuGetDefense v3.x is built only in .Net 6.0 so you will need the runtime/SDK installed.
 
 ## Unsupported Versions
+
 * Official Support follows support for the underlying framework.
-* Supporters can request support of unsupported versions (such as v2.x running on .Net 5) but are advised to use a supported runtime (for better overall security)
-  * .Net 5 projects can use 3.x as long as the .Net 6 runtime is installed.
-  
+* Supporters can request support of unsupported versions (such as v2.x running on .Net 5) but are advised to use a
+  supported runtime (for better overall security)
+    * .Net 5 projects can use 3.x as long as the .Net 6 runtime is installed.
+
 ## How does it work?
 
-  NuGetDefense is a bundled dotnet tool that runs using an [MSBuild ExecTask](https://docs.microsoft.com/en-us/visualstudio/msbuild/exec-task?view=vs-2019) after your project finishes building.
+NuGetDefense is a bundled dotnet tool that runs using
+an [MSBuild ExecTask](https://docs.microsoft.com/en-us/visualstudio/msbuild/exec-task?view=vs-2019) after your project
+finishes building.
 
 ## Love it? Support it
 
-You can sponsor this project on [Github](https://github.com/sponsors/digitalcoyote) and [Patreon](https://www.patreon.com/codingcoyote). The funds will be used to pay for software licenses and cloud/hardware costs that keep my projects running.
+You can sponsor this project on [Github](https://github.com/sponsors/digitalcoyote)
+and [Patreon](https://www.patreon.com/codingcoyote). The funds will be used to pay for software licenses and
+cloud/hardware costs that keep my projects running.
--- Src/NuGetDefenseTests/SqliteCacheTests.cs
+++ Src/NuGetDefenseTests/SqliteCacheTests.cs
@@ -6,87 +6,86 @@ using NuGetDefense;
 using NuGetDefense.Core;
 using Xunit;
 
-namespace NuGetDefenseTests
+namespace NuGetDefenseTests;
+
+public class SqliteCacheTests : IDisposable
 {
-    public class SqliteCacheTests : IDisposable
+    private const string TestSourceID = "TestSource";
+    private const string cacheFile = "./Test.sqlite";
+
+    public SqliteCacheTests()
     {
-        private const string TestSourceID = "TestSource";
-        private const string cacheFile = "./Test.sqlite";
+        if (File.Exists(cacheFile)) File.Delete(cacheFile);
+        SqlLiteVulnerabilityCache.CreateNewSqlLiteCache(cacheFile);
+    }
 
-        public SqliteCacheTests()
+    private SqlLiteVulnerabilityCache cache => new(cacheFile);
+
+    public void Dispose()
+    {
+        File.Delete(cacheFile);
+    }
+
+    [Fact]
+    public void UpdateCacheNoVulns()
+    {
+        const string packageId = "test_package";
+        const string packageVersion = "1.2.3-123Test";
+        var pkgs = new[]
         {
-            if (File.Exists(cacheFile)) File.Delete(cacheFile);
-            SqlLiteVulnerabilityCache.CreateNewSqlLiteCache(cacheFile);
-        }
+            new NuGetPackage { Version = packageVersion, Dependencies = Array.Empty<string>(), Id = packageId }
+        };
 
-        private SqlLiteVulnerabilityCache cache => new(cacheFile);
+        cache.UpdateCache(new(), pkgs, TestSourceID);
+        Dictionary<string, Dictionary<string, Vulnerability>> vulnDict = new();
+        cache.GetPackageCachedVulnerabilitiesForSource(pkgs[0], TestSourceID, ref vulnDict);
+        Assert.Empty(vulnDict);
+        cache.GetPackagesCachedVulnerabilitiesForSource(pkgs, TestSourceID, ref vulnDict);
+        Assert.Empty(vulnDict);
+        Assert.Empty(cache.GetUncachedPackages(pkgs, TimeSpan.FromDays(1), TestSourceID, out var cachedPkgs));
+        Assert.Collection(cachedPkgs, p => pkgs.Contains(p));
+    }
 
-        public void Dispose()
+    [Fact]
+    public void UpdateCacheWithVulnsCached()
+    {
+        const string packageId = "test_package";
+        const string packageVersion = "1.2.3-123Test";
+        var pkgs = new[]
         {
-            File.Delete(cacheFile);
-        }
+            new NuGetPackage { Version = "4.5.2", Dependencies = Array.Empty<string>(), Id = "no_vulns" },
+            new NuGetPackage { Version = packageVersion, Dependencies = Array.Empty<string>(), Id = packageId }
+        };
 
-        [Fact]
-        public void UpdateCacheNoVulns()
+        Dictionary<string, Dictionary<string, Vulnerability>> vulns = new()
         {
-            const string packageId = "test_package";
-            const string packageVersion = "1.2.3-123Test";
-            var pkgs = new[]
             {
-                new NuGetPackage { Version = packageVersion, Dependencies = Array.Empty<string>(), Id = packageId }
-            };
-
-            cache.UpdateCache(new(), pkgs, TestSourceID);
-            Dictionary<string, Dictionary<string, Vulnerability>> vulnDict = new();
-            cache.GetPackageCachedVulnerabilitiesForSource(pkgs[0], TestSourceID, ref vulnDict);
-            Assert.Empty(vulnDict);
-            cache.GetPackagesCachedVulnerabilitiesForSource(pkgs, TestSourceID, ref vulnDict);
-            Assert.Empty(vulnDict);
-            Assert.Empty(cache.GetUncachedPackages(pkgs, TimeSpan.FromDays(1), TestSourceID, out var cachedPkgs));
-            Assert.Collection(cachedPkgs, p => pkgs.Contains(p));
-        }
-
-        [Fact]
-        public void UpdateCacheWithVulnsCached()
-        {
-            const string packageId = "test_package";
-            const string packageVersion = "1.2.3-123Test";
-            var pkgs = new[]
-            {
-                new NuGetPackage { Version = "4.5.2", Dependencies = Array.Empty<string>(), Id = "no_vulns" },
-                new NuGetPackage { Version = packageVersion, Dependencies = Array.Empty<string>(), Id = packageId }
-            };
-
-            Dictionary<string, Dictionary<string, Vulnerability>> vulns = new()
-            {
+                pkgs[1].PackageUrl.ToLower(),
+                new()
                 {
-                    pkgs[1].PackageUrl.ToLower(),
-                    new()
                     {
-                        {
-                            "TestCVE",
-                            new("TestCVE", 1.0, "test", "TestDescription", new[] { "ref1", "ref2" }, Vulnerability.AccessVectorType.NETWORK, "TestVendor")
-                        }
+                        "TestCVE",
+                        new("TestCVE", 1.0, "test", "TestDescription", new[] { "ref1", "ref2" }, Vulnerability.AccessVectorType.NETWORK, "TestVendor")
                     }
                 }
-            };
-            cache.UpdateCache(vulns, pkgs, TestSourceID);
-            Dictionary<string, Dictionary<string, Vulnerability>> vulnDict = new();
-            Dictionary<string, Dictionary<string, Vulnerability>> vulnDict2 = new();
+            }
+        };
+        cache.UpdateCache(vulns, pkgs, TestSourceID);
+        Dictionary<string, Dictionary<string, Vulnerability>> vulnDict = new();
+        Dictionary<string, Dictionary<string, Vulnerability>> vulnDict2 = new();
 
-            cache.GetPackageCachedVulnerabilitiesForSource(pkgs[0], TestSourceID, ref vulnDict);
-            Assert.Empty(vulnDict);
+        cache.GetPackageCachedVulnerabilitiesForSource(pkgs[0], TestSourceID, ref vulnDict);
+        Assert.Empty(vulnDict);
 
-            cache.GetPackageCachedVulnerabilitiesForSource(pkgs[1], TestSourceID, ref vulnDict2);
-            Assert.Single(vulnDict2);
-            Assert.True(vulnDict2.ContainsKey(pkgs[1].PackageUrl), $"vulnDict2 does not contain key: {pkgs[1].PackageUrl}");
-            Assert.True(vulnDict2[pkgs[1].PackageUrl].ContainsKey("TestCVE"));
+        cache.GetPackageCachedVulnerabilitiesForSource(pkgs[1], TestSourceID, ref vulnDict2);
+        Assert.Single(vulnDict2);
+        Assert.True(vulnDict2.ContainsKey(pkgs[1].PackageUrl), $"vulnDict2 does not contain key: {pkgs[1].PackageUrl}");
+        Assert.True(vulnDict2[pkgs[1].PackageUrl].ContainsKey("TestCVE"));
 
-            cache.GetPackagesCachedVulnerabilitiesForSource(pkgs, TestSourceID, ref vulnDict);
-            Assert.True(vulnDict.Count == 1 && vulnDict.ContainsKey(pkgs[1].PackageUrl) && vulnDict[pkgs[1].PackageUrl].ContainsKey("TestCVE"));
+        cache.GetPackagesCachedVulnerabilitiesForSource(pkgs, TestSourceID, ref vulnDict);
+        Assert.True(vulnDict.Count == 1 && vulnDict.ContainsKey(pkgs[1].PackageUrl) && vulnDict[pkgs[1].PackageUrl].ContainsKey("TestCVE"));
 
-            Assert.Empty(cache.GetUncachedPackages(pkgs, TimeSpan.FromDays(1), TestSourceID, out var cachedPkgs));
-            Assert.True(cachedPkgs.All(p => pkgs.Contains(p)));
-        }
+        Assert.Empty(cache.GetUncachedPackages(pkgs, TimeSpan.FromDays(1), TestSourceID, out var cachedPkgs));
+        Assert.True(cachedPkgs.All(p => pkgs.Contains(p)));
     }
 }
