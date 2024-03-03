5e61b2633f6eb1a2c2823322c693ea61f22627a9
Curtis Carter <lokiphasma@gmail.com>
2024-03-03 14:03:52 -0600

    version bump from
    3.2.2.0

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -22,7 +22,7 @@ namespace NuGetDefense;
 
 public class Scanner
 {
-    public const string Version = "3.2.1.1";
+    public const string Version = "3.2.2.0";
     public const string UserAgentString = @$"NuGetDefense/{Version}";
     public const string DefaultSettingsFileName = "NuGetDefense.json";
     public const string DefaultVulnerabilityDataFileName = "VulnerabilityData.bin";
