bcb938dc4562d969c046b56b899506c60c028b5b
Curtis Carter <lokiphasma@gmail.com>
2024-04-24 22:00:55 -0500

    Bump NuGetDefense.Core
    Fixes support for VB.Net and F# projects

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -22,7 +22,7 @@ namespace NuGetDefense;
 
 public class Scanner
 {
-    public const string Version = "4.1.0-pre0001";
+    public const string Version = "4.1.0-pre0002";
     public const string UserAgentString = @$"NuGetDefense/{Version}";
     public const string DefaultSettingsFileName = "NuGetDefense.json";
     public const string DefaultVulnerabilityDataFileName = "VulnerabilityData.bin";
