db69cd8636b75c6bdba4b5fcd71a4d7991eea648
Curtis W Carter <lokiphasma@gmail.com>
2026-01-08 06:33:53 -0600

    bump package versions to 5.0

--- Src/NuGetDefense.Lib/Scanner.cs
+++ Src/NuGetDefense.Lib/Scanner.cs
@@ -22,7 +22,7 @@ namespace NuGetDefense;
 
 public class Scanner
 {
-    public const string Version = "4.1.4.0";
+    public const string Version = "5.0.0.0";
     public const string UserAgentString = @$"NuGetDefense/{Version}";
     public const string DefaultSettingsFileName = "NuGetDefense.json";
     public const string DefaultVulnerabilityDataFileName = "VulnerabilityData.bin";
